9c54d4817e308ad447a75cc571d2f35729678e57
kulbhushan singh <kulbhushansingh919@gmail.com>
2024-09-30 22:55:18 +0530

    if-else-statement

---
+++ Learn the Basics/IF-Else-Statement/first.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namcespace std;
+
+int main(){
+    /*** Write a Program for age calculation wit the using
+      IF-ELSE-STATEMENT 
+     */
+ṇ
+    int age;
+    cin>> age; ṇ
+
+    if(age >= 18){
+        cout << "You are an adult";
+    }
+    else if(age < 18){
+        cout << "You are not an Adult";
+    }
+
+    return 0;
+}
